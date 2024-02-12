8dfcb7c8e0c0532d1fccfc2bb67b14b049bafd52
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-02-12 21:35:23 +0530

    open/close hamburger

--- shared.js
+++ shared.js
@@ -2,6 +2,9 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
     backdrop.style.display = "block";
@@ -9,9 +12,16 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
   });
 }
 modalNoButton.addEventListener("click", closeModal);
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
 
 function closeModal() {
   backdrop.style.display = "none";
   modal.style.display = "none";
 }
+toggleButton.addEventListener("click", function () {
+  backdrop.style.display = "block";
+  mobileNav.style.display = "block";
+});
