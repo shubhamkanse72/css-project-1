a2fc6694043dbfca1db76efeda89b35e22e97ebc
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-02-12 21:26:36 +0530

    modal/backdrop css

--- shared.js
+++ shared.js
@@ -1,10 +1,17 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var selectPlanButtons = document.querySelectorAll('.plan button');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var modalNoButton = document.querySelector(".modal__action--negative");
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    backdrop.style.display = "block";
+    modal.style.display = "block";
+  });
+}
+modalNoButton.addEventListener("click", closeModal);
+backdrop.addEventListener("click", closeModal);
 
-for(var i=0; i<selectPlanButtons.length;i++){
-    selectPlanButtons[i].addEventListener('click', function(){
-        backdrop.style.display='block';
-        modal.style.display='block';
-    });
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
 }
