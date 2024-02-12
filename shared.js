48865be2a7d01ba7bd2263134aab2da5f3c4da6b
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-02-12 22:06:10 +0530

    added backdrop/hamburger

--- shared.js
+++ shared.js
@@ -7,21 +7,32 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.style.display = "block";
+    backdrop.classList.add("open");
+    modal.classList.add("open");
   });
 }
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
 backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none";
+  //mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
   closeModal();
 });
 
 function closeModal() {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  backdrop.classList.remove("open");
+  if (modal) {
+    modal.classList.remove("open");
+  }
 }
 toggleButton.addEventListener("click", function () {
-  backdrop.style.display = "block";
-  mobileNav.style.display = "block";
+  //backdrop.style.display = "block";
+  // mobileNav.style.display = "block";
+  backdrop.classList.add("open");
+  mobileNav.classList.add("open");
 });
